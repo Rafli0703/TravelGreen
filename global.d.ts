69ceb496ab9fd154e00f73c6ae39d180ec2e8b3c
Rafli Saputra <62581878+Rafli0703@users.noreply.github.com>
2025-11-27 12:49:03 +0700

    Add files via upload

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+
+
+
+declare module '*.css';
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['var(--font-body)'],
+        heading: ['var(--font-heading)'],
+      },
+    },
+  },
+  plugins: [],
+}
